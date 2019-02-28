a7d9216aef391b0c7798d1394918cf61efa14b88
Julio Lopez <julioj.lopeza@gmail.com>
2019-02-27 23:23:30 -0300

    working with components

---
+++ src/components/Intro.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class Intro extends React.Component{
+    public render(){
+        return (
+            <p className="App-intro">
+                To get started, edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        )
+    }
+}
